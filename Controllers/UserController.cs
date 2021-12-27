c94cee42ab30715cb89101686dc5b9e059de6a60
2100663_Jason <2100663@systex.com>
2021-12-27 14:00:36 +0800

    1227 實習計畫第四階段 Strarup.cs TOKEN驗證邏輯

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Article_Backend.Controllers
 {
@@ -20,7 +24,96 @@ namespace Article_Backend.Controllers
             _configuration = configuration;
         }
 
-        // [HttpPost("login")]
+        [HttpPost("login")]
+        public Response<string> PostLogin(Account user)
+        {
+            Response<string> result = new Response<string>();
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    result.StatusCode = Status.BadRequest;
+                    result.Message = nameof(Status.BadRequest);
+                    result.Data = null;
+                    return result;
+                }
+
+                string conn = _configuration.GetValue<string>("ConnectionStrings:DevConnection");
+                using (SqlConnection connection = new SqlConnection(conn))
+                {
+                    string queryString = @"select [Id], [Name], [Status] 
+                                            from [ArticleDB].[dbo].[User] 
+                                            where [Name]=@Name 
+                                            and [Password]=@Password";
+
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.AddRange(new SqlParameter[]
+                    {
+                        new SqlParameter("@Name", user.UserName),
+                        new SqlParameter("@Password", user.Password),
+                    });
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if(!reader.HasRows)
+                    {
+                        result.StatusCode = Status.NotFound;
+                        result.Message = nameof(Status.NotFound);
+                        result.Data = null;
+                        return result;
+                    }
+                    UserDetail userDetail = new UserDetail();
+                    if(reader.Read())
+                    {
+                        userDetail.Id = reader.GetInt32("Id");
+                        userDetail.Name = reader.GetString("Name");
+                        userDetail.Status = reader.GetInt32("Status");
+                        Console.WriteLine($"content: {userDetail.Id} and {userDetail.Name} and {userDetail.Status}");
+                    }
+                    connection.Close();
+                    byte[] key = Encoding.ASCII.GetBytes(_configuration["JwtSetting:Key"]);
+                    SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new Claim[]
+                        {
+                            new Claim("Id",userDetail.Id.ToString(),ClaimValueTypes.Integer32),
+                            new Claim("Name",userDetail.Name),
+                            new Claim("Status",userDetail.Status.ToString(),ClaimValueTypes.Integer32)
+                        }),
+                        Expires = DateTime.Now.AddHours(1),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                    SecurityToken securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                    string token = tokenHandler.WriteToken(securityToken);
+                    Console.WriteLine($"jwt token: {token}");
+                    string queryString1 = @"update [ArticleDB].[dbo].[Token] 
+                                            set [Token]=@Token, [UpdateDatetime]=GETUTCDATE() 
+                                            where [User_Id]=@User_Id";
+                    SqlCommand command1 = new SqlCommand(queryString1, connection);
+                    command1.Parameters.AddRange(new SqlParameter[]
+                    {
+                        new SqlParameter("@Token", token),
+                        new SqlParameter("@User_Id", userDetail.Id)
+                    });
+                    connection.Open();
+                    int check = command1.ExecuteNonQuery();
+                    Console.WriteLine("check is update or not? "+check);
+                    connection.Close();
+                    result.StatusCode = Status.OK;
+                    result.Message = nameof(Status.OK);
+                    result.Data = token;
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e.Message);
+                result.StatusCode = Status.SystemError;
+                result.Message = nameof(Status.SystemError);
+                result.Data = null;
+                return result;
+            }
+        }
 
 
         [HttpPost("sign")]
---
+++ temp.cs
@@ -0,0 +1,6 @@
+// Console.WriteLine("隔空取物");
+// Console.WriteLine(HttpContext.Items.TryGetValue("Token", out object n));
+// Console.WriteLine(n);
+// Console.WriteLine(HttpContext.Items["Token"]);
+// Console.WriteLine(JsonConvert.SerializeObject(HttpContext.Items["Token"]));
+
